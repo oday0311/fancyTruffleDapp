a80bedf392030bfe7eeb577408081e4ac1e24cf9
alex <oday0311@hotmail.com>
2023-03-27 16:49:58 +0800

    update..

--- contracts/HelloBlockchain.sol
+++ contracts/HelloBlockchain.sol
@@ -1,47 +1,29 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.25 <0.9.0;
 
 contract HelloBlockchain {
-    enum StateType { Request, Respond }
-
-    StateType public  State;
-    address public  Requestor;
-    address public  Responder;
-
-    string public RequestMessage;
-    string public ResponseMessage;
+    string public data;
 
     constructor(string memory message) {
-        Requestor = msg.sender;
-        RequestMessage = message;
-        State = StateType.Request;
+        data = message;
     }
 
     // call this function to send a request
     function SendRequest(string memory requestMessage) public {
-
-        if (Requestor != msg.sender) {
-            revert();
-        }
-
-        RequestMessage = requestMessage;
-        State = StateType.Request;
+        data = requestMessage;
     }
 
     // call this function to send a response
     function SendResponse(string memory responseMessage) public  returns (string memory){
-
-        Responder = msg.sender;
-        ResponseMessage = responseMessage;
-        State = StateType.Respond;
-
+        data = responseMessage;
         return "hello";
     }
 
-    function GetMessage() public pure returns (string memory) {
-        return "hello";
+    function GetMessage() public view  returns (string memory) {
+        return data;
     }
 
-    function say() public pure returns (string memory) {
-        return "hello";
+    function say() public view  returns (string memory) {
+        return data;
     }
 }
